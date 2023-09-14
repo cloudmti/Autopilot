0d869fb2e5ee7d2565a11d00b080030b9ed1569d
cloudmti <145045992+cloudmti@users.noreply.github.com>
2023-09-14 18:50:11 -0300

    Delete AutopilotEnrollment.ps1

--- AutopilotEnrollment.ps1
+++
@@ -1,8 +0,0 @@
-#Variables
-$TenantID="43dfbc96-d87e-4966-8414-cef7aef79ec6"
-$AppID="fb9d9b63-e7df-44d5-af77-c3ef7ba17ccb"
-$AppIDSecretValor="Q5p8Q~24FQRt~w33._DXQ4I1foGOcT_pLAtSrbHF"
-#RegisterDevice
-Set-ExecutionPolicy Unrestricted -Force
-Install-Script get-windowsautopilotinfo -Force
-get-windowsautopilotinfo.ps1 -online -TenantID $TenantID -appid $AppID -appsecret $AppIDSecretValor
